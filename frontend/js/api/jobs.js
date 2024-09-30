5c874ce689b36e75ff521aef77e98cf559a88597
LaceyMarieS <122472928+LaceyMarieS@users.noreply.github.com>
2024-09-30 16:13:43 -0600

    getJobs function created
    now working on rendering jobs in DOM

--- frontend/js/api/jobs.js
+++ frontend/js/api/jobs.js
@@ -1,1 +1,6 @@
 // your code goes here.
+
+export async function getJobs(url = 'http://localhost:3000/jobs'){
+    const res = await fetch(url)
+    return await res.json()
+}
--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,1 +1,16 @@
 // your code goes here.
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { getJobs } from './api/jobs';
+
+document.querySelector("#search-jobs-form").addEventListener("submit", (e) => {
+    e.preventDefault()
+    const search =  document.querySelector("#query-input").value
+    render(`http://localhost:3000/jobs?search=${search}`)
+});
+
+function render(url){
+    getJobs(url).then((data) =>{
+        console.log(data)
+    })
+}
